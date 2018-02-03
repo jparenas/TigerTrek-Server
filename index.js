43844d73fed508d76ce68a4d8decb680116881c8
jparenas <juan.p.arenas1@gmail.com>
2018-02-03 15:26:30 -0500

    Fixed a port error

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(helmet())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = 3306
+const port = process.env.PORT || 8080;
 
 var authenticationCache = []
 
